7c91fba913a2a308d2dbc2ee1a9c621e755e32e1
sumitra230407 <137391572+sumitra230407@users.noreply.github.com>
2024-10-16 12:55:22 +0800

    Add files via upload

--- snorkeling.js
+++ snorkeling.js
@@ -6,13 +6,6 @@ window.addEventListener("scroll", () => {
     console.log('Scroll Y:', top);
 
     listBg.forEach((bg, index) => {
-        // // Pastikan bg-1 tidak bergerak
-        // if (index === 8) {
-        //     // Jangan lakukan apa-apa, biarkan posisi tetap untuk bg-1
-        //     return;
-        // }
-
-        // Lanjutkan transformasi untuk elemen lainnya
         if (index != 8 && index != 7 && index != 6 && index != 5 && index != 4 && index != 3 && index != 2) {
             bg.style.transform = `translateY(${(top * index / 8)}px)`;
         }else if (index === 2) {
